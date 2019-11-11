17c278c1940c94e1ee2f1a4f2491f2837b975148
Lee Morgan <Morgan.LeeR@protonmail.com>
2019-11-11 17:04:12 +0300

    Add ability to click on transactions and view the data

--- views/financePage/finance.js
+++ views/financePage/finance.js
@@ -1,8 +1,16 @@
 // console.log(user);
 
 let financePage = {
-    displayTransactions: function(){
-        let tbody = document.querySelector("tbody");
+    home: document.querySelector(".home"),
+    transaction: document.querySelector(".transaction"),
+
+    //Displays the main page
+    //Populates the table with transactions
+    displayHome: function(){
+        this.home.style.display = "flex";
+        this.transaction.style.display = "none";
+
+        let tbody = document.querySelector(".home tbody");
 
         while(tbody.children.length > 0){
             tbody.removeChild(tbody.firstChild);
@@ -11,6 +19,8 @@ let financePage = {
         for(let transaction of user.account.transactions){
             let transactionDate = new Date(transaction.date);
             let row = document.createElement("tr");
+            row.onclick = ()=>{this.singleTransaction(transaction._id);};
+            row.classList = "transRow";
             tbody.appendChild(row);
 
             let date = document.createElement("td");
@@ -22,11 +32,33 @@ let financePage = {
             row.appendChild(location);
 
             let amount = document.createElement("td");
-            amount.innerText = `$${transaction.amount.toFixed(2)}`;
+            amount.innerText = `$${Number(transaction.amount).toFixed(2)}`;
             row.appendChild(amount);
         }
     },
 
+    //Detailed display of a single transaction
+    singleTransaction: function(id){
+        this.home.style.display = "none";
+        this.transaction.style.display = "block";
+
+        let currentTransaction = {};
+        for(let transaction of user.account.transactions){
+            if(transaction._id === id){
+                currentTransaction = transaction;
+                break;
+            }
+        }
+
+        document.querySelector("#transDate").innerText = new Date(currentTransaction.date).toDateString();
+        document.querySelector("#transCategory").innerText = currentTransaction.category;
+        document.querySelector("#transLocation").innerText = currentTransaction.location;
+        document.querySelector("#transAmount").innerText = `$${currentTransaction.amount}`;
+        document.querySelector("#transNote").innerText = currentTransaction.note;
+    },
+
+    //Axios request to create new transaction in database
+    //Adds the transaction to the transaction list
     newTransaction: function(){
         event.preventDefault();
         let transaction = {
@@ -42,13 +74,13 @@ let financePage = {
                 user.account.balance = (Number(user.account.balance) + Number(transaction.amount)).toFixed(2).toString();
                 document.querySelector("#balance").innerText = user.account.balance;
                 user.account.transactions.unshift(transaction);
-                this.displayTransactions();
+                this.displayHome();
             })
             .catch((err)=>{
                 console.log(err);
-                banner.createError("Looks like there has been an oopsie");
+                banner.createError("Looks like there's been an oopsie");
             });
     }
 }
 
-financePage.displayTransactions();
+financePage.displayHome();
